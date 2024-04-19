cb970768c8ae369a6f54fece8ca8c0b1ed8699c8
Kolya <karpushen01082004@gmail.com>
2024-04-19 23:12:46 +0300

    Registration Page

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_04_19_113547) do
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "name"
