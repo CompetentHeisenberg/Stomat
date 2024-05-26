708af483c6ba2e12abceb53a7c10ea8375ad0a8d
Kolya <karpushen01082004@gmail.com>
2024-05-26 23:24:22 +0300

    Fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,6 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_secure_password
   validates :email, presence: true, uniqueness: true
-  validates :name, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z]+\z/}
-  #validates :password length: { minimum: 8 }
-            #format: { with: /\A(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}\z/}
+  validates :name, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/ }
+  validates :password, length: { minimum: 8 }, if: -> { new_record? || !password.nil? }
 end
